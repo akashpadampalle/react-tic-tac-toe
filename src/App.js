b48c58b014fad048901e7f4a7515bb793d1fa3c3
Akash Padampalle <akashpadampalle780@gmail.com>
2023-05-13 18:38:02 +0530

    break 1

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export function Square() {
+
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>)
+}
+
+export default function Board() {
+  return (
+    <React.Fragment>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+    </React.Fragment>
+  );
+}
