1bcb04ddce8b0b82f2fd48f233eaaa8cc602f638
Akash Padampalle <akashpadampalle780@gmail.com>
2023-05-14 08:41:56 +0530

    complete basic tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,44 +1,88 @@
 import React, { useState } from "react";
 
-export function Square() {
+export function Square({ value, onSqureClick }) {
 
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
 
   return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
+    <button className="square" onClick={onSqureClick}>
       {value}
     </button>)
 }
 
 export default function Board() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
+    const nextSquers = squares.slice();
+    if (xIsNext) {
+      nextSquers[i] = "X";
+    } else {
+      nextSquers[i] = "O"
+    }
+    setSquares(nextSquers);
+    setXIsNext(!xIsNext);
+
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next player is " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <React.Fragment>
 
+      <div className="status">{status}</div>
+
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSqureClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSqureClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSqureClick={() => handleClick(2)} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSqureClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSqureClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSqureClick={() => handleClick(5)} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSqureClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSqureClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSqureClick={() => handleClick(8)} />
       </div>
 
     </React.Fragment>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
